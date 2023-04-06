7351e9b247616acdf7e1f0c31d288919f43c7c04
flash238116 <129646401+flash238116@users.noreply.github.com>
2023-04-06 21:48:44 +0530

    Update coc.py

--- coc.py
+++ coc.py
@@ -20,136 +20,136 @@ token='eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03Z
 
 response=requests.get(url,headers={'Accept':'application/json' ,'authorization': 'Bearer ' + token})
 cwl=response.json()
-cwl
+print(cwl)
 
 
-# In[41]:
+# # In[41]:
 
 
-cwl_df=pd.DataFrame()
-cwl_season=cwl['season']
-for i in cwl['clans']:
-    temp=pd.DataFrame(data=i['members'])
-    temp['clantag']=i['tag']
-    temp['clanname']=i['name']
-    temp['clanlevel']=i['clanLevel']
-    temp['clanbadge']=i['badgeUrls']['large']
-    temp['clanseason']=cwl_season
-    cwl_df=pd.concat([cwl_df,temp])
-cwl_df.reset_index(drop=True,inplace=True)
-cwl_df.sort_values(by=['clantag','tag'],inplace=True,ignore_index=True)
-cwl_df
+# cwl_df=pd.DataFrame()
+# cwl_season=cwl['season']
+# for i in cwl['clans']:
+#     temp=pd.DataFrame(data=i['members'])
+#     temp['clantag']=i['tag']
+#     temp['clanname']=i['name']
+#     temp['clanlevel']=i['clanLevel']
+#     temp['clanbadge']=i['badgeUrls']['large']
+#     temp['clanseason']=cwl_season
+#     cwl_df=pd.concat([cwl_df,temp])
+# cwl_df.reset_index(drop=True,inplace=True)
+# cwl_df.sort_values(by=['clantag','tag'],inplace=True,ignore_index=True)
+# cwl_df
 
 
-# In[18]:
+# # In[18]:
 
 
-war_df=pd.DataFrame()
-for x in cwl['rounds']:
-    for y in x['warTags']:
-        warTag=re.sub('#','',y)
-        url2='https://api.clashofclans.com/v1/clanwarleagues/wars/%23'+warTag
-        response=requests.get(url2,headers={'Accept':'application/json' ,'authorization': 'Bearer ' + token})
-        cwl_war=response.json() 
-        if cwl_war['state']=='inWar' or cwl_war['state']=='warEnded':
-            for i in ['clan','opponent']:
-                for j in cwl_war[i]['members']:
-                    try:
-                        attacks=pd.DataFrame(data=j['attacks'])
-                        attacks['clanTag']=cwl_war[i]['tag']
-                        attacks['teamSize']=cwl_war['teamSize']
-                        attacks['preparationStartTime']=cwl_war['preparationStartTime']
-                        attacks['startTime']=cwl_war['startTime']
-                        attacks['endTime']=cwl_war['endTime']
-                        attacks['warTag']=warTag
-                    except KeyError:
-                        attacks=pd.DataFrame()
-                    war_df=pd.concat([war_df,attacks])
-war_df.reset_index(drop=True,inplace=True)
-war_df['preparationStartTime']=pd.to_datetime(war_df['preparationStartTime'])
-war_df['startTime']=pd.to_datetime(war_df['startTime'])
-war_df['endTime']=pd.to_datetime(war_df['endTime'])
-war_df
+# war_df=pd.DataFrame()
+# for x in cwl['rounds']:
+#     for y in x['warTags']:
+#         warTag=re.sub('#','',y)
+#         url2='https://api.clashofclans.com/v1/clanwarleagues/wars/%23'+warTag
+#         response=requests.get(url2,headers={'Accept':'application/json' ,'authorization': 'Bearer ' + token})
+#         cwl_war=response.json() 
+#         if cwl_war['state']=='inWar' or cwl_war['state']=='warEnded':
+#             for i in ['clan','opponent']:
+#                 for j in cwl_war[i]['members']:
+#                     try:
+#                         attacks=pd.DataFrame(data=j['attacks'])
+#                         attacks['clanTag']=cwl_war[i]['tag']
+#                         attacks['teamSize']=cwl_war['teamSize']
+#                         attacks['preparationStartTime']=cwl_war['preparationStartTime']
+#                         attacks['startTime']=cwl_war['startTime']
+#                         attacks['endTime']=cwl_war['endTime']
+#                         attacks['warTag']=warTag
+#                     except KeyError:
+#                         attacks=pd.DataFrame()
+#                     war_df=pd.concat([war_df,attacks])
+# war_df.reset_index(drop=True,inplace=True)
+# war_df['preparationStartTime']=pd.to_datetime(war_df['preparationStartTime'])
+# war_df['startTime']=pd.to_datetime(war_df['startTime'])
+# war_df['endTime']=pd.to_datetime(war_df['endTime'])
+# war_df
 
 
-# In[ ]:
+# # In[ ]:
 
 
-url3='https://api.clashofclans.com/v1/clans/%232LC9VR0JC/warlog'
-response=requests.get(url,headers={'Accept':'application/json' ,'authorization': 'Bearer ' + token})
-wl=response.json()
+# url3='https://api.clashofclans.com/v1/clans/%232LC9VR0JC/warlog'
+# response=requests.get(url,headers={'Accept':'application/json' ,'authorization': 'Bearer ' + token})
+# wl=response.json()
 
 
-# In[22]:
+# # In[22]:
 
 
-connection_string='mongodb+srv://flash238116:238116flash@activewarriors.1jmpfkt.mongodb.net/?retryWrites=true&w=majority'
-client = MongoClient(connection_string)
+# connection_string='mongodb+srv://flash238116:238116flash@activewarriors.1jmpfkt.mongodb.net/?retryWrites=true&w=majority'
+# client = MongoClient(connection_string)
 
 
-# In[23]:
+# # In[23]:
 
 
-db=client['cwl']
-collection1 = db['seasoninfo']
-collection2 = db['warinfo']
+# db=client['cwl']
+# collection1 = db['seasoninfo']
+# collection2 = db['warinfo']
 
 
-# In[42]:
+# # In[42]:
 
 
-seasoninfo=pd.DataFrame(data=collection1.find())
-seasoninfo=seasoninfo.iloc[:,1:]
-seasoninfo.sort_values(by=['clantag','tag'],inplace=True,ignore_index=True)
-seasoninfo
+# seasoninfo=pd.DataFrame(data=collection1.find())
+# seasoninfo=seasoninfo.iloc[:,1:]
+# seasoninfo.sort_values(by=['clantag','tag'],inplace=True,ignore_index=True)
+# seasoninfo
 
 
-# In[46]:
+# # In[46]:
 
 
-warinfo=pd.DataFrame(data=collection2.find())
-warinfo=warinfo.iloc[:,1:]
-warinfo.sort_values(by=['clanTag','attackerTag'],inplace=True,ignore_index=True)
-warinfo
+# warinfo=pd.DataFrame(data=collection2.find())
+# warinfo=warinfo.iloc[:,1:]
+# warinfo.sort_values(by=['clanTag','attackerTag'],inplace=True,ignore_index=True)
+# warinfo
 
 
-# In[83]:
+# # In[83]:
 
 
-check1=cwl_df.merge(seasoninfo,how='left',on=['tag','clantag','clanseason'],suffixes=['','_y'])
-check1=check1.loc[check1.name_y.isna()][['tag', 'name', 'townHallLevel', 'clantag', 'clanname', 'clanlevel',
-       'clanbadge', 'clanseason']]
-check1.reset_index(inplace=True,drop=True)
+# check1=cwl_df.merge(seasoninfo,how='left',on=['tag','clantag','clanseason'],suffixes=['','_y'])
+# check1=check1.loc[check1.name_y.isna()][['tag', 'name', 'townHallLevel', 'clantag', 'clanname', 'clanlevel',
+#        'clanbadge', 'clanseason']]
+# check1.reset_index(inplace=True,drop=True)
 
 
-check2=war_df.merge(warinfo,how='left',on=['warTag','attackerTag'],suffixes=['','_y'])
-check2=check2.loc[check2.defenderTag_y.isna()][['attackerTag', 'defenderTag', 'stars', 'destructionPercentage', 'order',
-       'duration', 'clanTag', 'teamSize', 'preparationStartTime', 'startTime',
-       'endTime', 'warTag']]
-check2.reset_index(inplace=True,drop=True)
+# check2=war_df.merge(warinfo,how='left',on=['warTag','attackerTag'],suffixes=['','_y'])
+# check2=check2.loc[check2.defenderTag_y.isna()][['attackerTag', 'defenderTag', 'stars', 'destructionPercentage', 'order',
+#        'duration', 'clanTag', 'teamSize', 'preparationStartTime', 'startTime',
+#        'endTime', 'warTag']]
+# check2.reset_index(inplace=True,drop=True)
 
 
-# In[84]:
+# # In[84]:
 
 
-if check1.empty==False:
-    check1=check1.to_dict('records')
-    collection1.insert_many(check1)
+# if check1.empty==False:
+#     check1=check1.to_dict('records')
+#     collection1.insert_many(check1)
 
-if check2.empty==False:
-    check2=check2.to_dict('records')
-    collection2.insert_many(check2)
-
-
-# In[109]:
+# if check2.empty==False:
+#     check2=check2.to_dict('records')
+#     collection2.insert_many(check2)
 
 
-war_df
+# # In[109]:
 
 
-# In[87]:
+# war_df
 
 
-import sys
-get_ipython().system('{sys.executable} -m pip freeze')
+# # In[87]:
+
+
+# import sys
+# get_ipython().system('{sys.executable} -m pip freeze')
 
